8e723a81bb05bfa8af413ad3c9ad4cb4822b23b3
OneWay <839357821@qq.com>
2020-11-01 01:35:33 +0800

    创建参数相关的功能

--- aiutils/shutils.py
+++ aiutils/shutils.py
@@ -49,3 +49,60 @@ def zipDir(tar,excludes=None):
         for ex in excludes:
             sh+=" -x \"{}\"".format(ex)
     executeSh(sh)
+
+param={
+    "num_epoch":"5",
+    "batch_size":"32",
+    "use_gpu":"True",
+    "checkpoint_dir":"",
+    "learning_rate":"3e-5",
+    "max_seq_len":"256",
+    "worktype":"train",
+    "modelname":"roberta_wwm_ext_chinese_L-24_H-1024_A-16",
+    "predictfile":"test.json",
+    "predsign":"pred",
+    "trainfile":"train.json",
+    "trainfileex":"",
+    "devfile":"dev.json",
+    "warmup_proportion":"0.1",
+    "weight_decay":"0.01",
+    "use_data_parallel":"True"
+}
+
+def cloneParam(param):
+    obj={}
+    for key in param:
+        obj[key]=param[key]
+    return obj
+
+def createParams(param,df):
+    rst={}
+    for key,value in df.items():
+        if key in param:
+            rst[key]=param[key]
+        else:
+            rst[key]=value
+    rsts=[]
+    for key,value in rst.items():
+        t="--{}={}".format(key,value)
+        rsts.append(t)
+    return " ".join(rsts)
+
+def createPythonCall(py,param,df):
+    paramStr=createParams(param,df)
+    temp="!python {} {}".format(py,paramStr)
+    return temp
+
+def createPythonCalls(py,params,df):
+    rst=[]
+    for param in params:
+        rst.append(createPythonCall(py,param,df))
+    return rst
+
+def createModelParams(modelTemp,count):
+    rst=[]
+    for i in range(count):
+        t={}
+        t["checkpoint_dir"]=modelTemp.format(str(i))
+        rst.append(t)
+    return rst
